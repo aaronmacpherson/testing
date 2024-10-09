9f2d8291b33e339a67bb89d32a40f1374c83b6a4
aaron <aaronmacpherson@outlook.com>
2024-10-09 12:22:11 -0300

    end lesson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,26 @@
 
 import Banner from "@/components/Banner";
 import OrdersReport from "@/components/OrdersReport";
+import { useState } from "react";
+
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function Home() {
+
+  // ---------------------------- event handlers
+  const showOrders = (e:any) => setAppState(2);
+
+  // ---------------------------- state variables
+  // 1 - no orders loaded
+  // 2 - orders loading
+  // 3 - orders have been loaded
+  const [appState, setAppState] = useState(1);
 
   // ---------------------------- rendering to DOM
   return (
     <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
+
+      <LoadingOverlay show={(appState == 2) ? true : false } bgColor="#b82308" />
 
       <Banner />
 
@@ -18,8 +32,9 @@ export default function Home() {
         <div>
           <div className="text-accent text-3xl font-bold mb-2.5">Welcome loyal pizza dispatcher....</div>Click the &quot;Get Orders&quot; button below to view all current orders that need to be delivered.
           <div>
-              <button 
-                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders</button>
+              <button onClick={showOrders}
+                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders
+              </button>
           </div>
         </div>
         <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">
@@ -34,7 +49,7 @@ export default function Home() {
 
         <div id="output" className="divide-dashed divide-y-2 divide-accent">
 
-          <OrdersReport />
+          <OrdersReport setAppState={setAppState} appState={appState} />
 
         </div>
       </div>
